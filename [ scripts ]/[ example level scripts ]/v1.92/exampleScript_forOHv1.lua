808c52f0066aa4ff16e0d5824abcaadfbbe51345
Marchionne Evangelisti <60834021+C-Viridian-19053@users.noreply.github.com>
2022-10-07 17:38:51 +0700

    update example script for OH v1.92

--- [ scripts ]/[ example level scripts ]/v1.92/exampleScript_forOHv1.lua
+++ [ scripts ]/[ example level scripts ]/v1.92/exampleScript_forOHv1.lua
@@ -20,6 +20,7 @@ local march31oPatternSpawnAmount = 0;
 -- configs can be changed (IMPORTANT FOR OH 2.0.2+ USERS: Delete the current replay file before changing the config)
 march31oPat_timesDo = 999; --cycle do (unused, maybe)
 march31oPat_thickness = 40; --thickness tho
+march31oPatDel_isRebootingSide = false; --reboot side if target side has same (FOR PATTERNS ONLY) (optional)
 march31oPatDel_AdditionalDelay = 0; --delay increment after patterns loaded (FOR PATTERNS ONLY) (required)
 march31oPatDel_AddMult = 1; --additional delay mult, alternation of delay multiplier...i think (FOR PATTERNS ONLY) (required)
 -- delay config (recommended)
@@ -43,12 +44,12 @@ function spawnPattern(mNumbSpawn)
         mNumbSpawn = 5
     end
 
-        if mNumbSpawn == 0 then pMarch31osAlternatingBarrage(_side, march31oPat_thickness, math.random(3, 6), false, 0, 0, 2, false, 1, 1, getRandomDir(), true, march31oPatDel_AdditionalDelay, march31oPatDel_AddMult)
-    elseif mNumbSpawn == 1 then pMarch31osWhirlwind(_side, math.random(3, 6), 0, math.floor(getProtocolSides() / 3), 1, false, 1, 1, getRandomDir(), 0, 0, false, true, march31oPatDel_AdditionalDelay, march31oPatDel_AddMult)
-    elseif mNumbSpawn == 2 then pMarch31osBarrageSpiral(_side, march31oPat_thickness, math.random(0, 3), 1, 1, 1, getRandomDir(), true, march31oPatDel_AdditionalDelay, march31oPatDel_AddMult)
-    elseif mNumbSpawn == 3 then pMarch31osBarrageReversals(_side, march31oPat_thickness, 1, false, 1, 1, math.random(0, 1), true, march31oPatDel_AdditionalDelay, march31oPatDel_AddMult)
-    elseif mNumbSpawn == 4 then pMarch31osTunnel(_side, march31oPat_thickness * 1.75, nil, math.random(1, 3), 2, 0, 2, true, false, 0, 0, 1, 1, false, 0, 2, false, 1, nil, 0, nil, 1, 1, math.random(0, 1), true, march31oPatDel_AdditionalDelay, march31oPatDel_AddMult)
-    elseif mNumbSpawn == 5 then pMarch31osWhirlwind(_side, getProtocolSides() * math.random(1, 2), 0, 1, 1, false, 1, 1, getRandomDir(), 0, 0, false, true, march31oPatDel_AdditionalDelay, march31oPatDel_AddMult);
+        if mNumbSpawn == 0 then pMarch31osAlternatingBarrage(_side, march31oPat_thickness, math.random(3, 6), true, false, 0, 0, 2, false, 1, 1, getRandomDir())
+    elseif mNumbSpawn == 1 then pMarch31osWhirlwind(_side, math.random(3, 6), 0, math.floor(getProtocolSides() / 3), 1, false, 1, 1, getRandomDir(), 0, 0, false)
+    elseif mNumbSpawn == 2 then pMarch31osBarrageSpiral(_side, march31oPat_thickness, math.random(0, 3), 1, 1, false, 1, 1, getRandomDir())
+    elseif mNumbSpawn == 3 then pMarch31osBarrageReversals(_side, march31oPat_thickness, 1, false, 1, 1, math.random(0, 1))
+    elseif mNumbSpawn == 4 then pMarch31osTunnel(_side, march31oPat_thickness * 1.75, nil, math.random(1, 3), 1, 1, 1, u_rndInt(0, 1))
+    elseif mNumbSpawn == 5 then pMarch31osWhirlwind(_side, getProtocolSides() * math.random(1, 2), 0, 1, 1, false, 1, 1, getRandomDir(), 0, 0, false);
     end
 
     -- debug, remove if you don't want to use this
